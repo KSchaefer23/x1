3a72cb64f1362562af553d32726acea0e99a417c
KSchaefer23 <kvnschaefer@yahoo.com>
2015-09-09 21:30:27 -0400

    X1 Update 2

--- x1.java
+++ x1.java
@@ -1,10 +1,11 @@
-//////// What am I trying to do???
-//////// What is my name?  (CST 112; today's date?)
+//////// Draw Scenery
+//////// Kevin Schaefer  (CST 112 EVE 9/9/15)
 
 //// GLOBALS:  coordinates, speed, etc.
 float x, y;       // Position of creature.
 float dx, dy;     // Speed.
 float horizon;
+float sunX, sunY;
 
 //// SETUP:  window size, initialization (start in middle of screen).
 void setup() {
@@ -14,6 +15,8 @@ void setup() {
   y=  height/2;
   dx=  3;
   dy=  2;
+  sunX= width*3/4;
+  sunY= height/8;
 }
 
 //// NEXT FRAME:  scene, action, show.
@@ -21,7 +24,11 @@ void draw() {
   //// SCENE:  sky, sun, tree, house, etc.
   background( 100,150,200 );                // sky
   fill( 255,255,0 );
-  ellipse( width*3/4, height/8, 40,40 );    // sun
+  
+  //sunX= (sunX + 1) % (width+100);
+  //sunY= (sunY + 1) % height;
+  ellipse( width*3/4, height/8, 60,60 );    // sun
+  //ellipse(sunX,sunY, 60,60 );    // sun
   // Grass
   fill( 100,200,100 );
   rect( 0,horizon, width,height*3/4 );      // grass.
@@ -34,9 +41,13 @@ void draw() {
   rect(140, horizon-10, 20,10 );
   
   rect(30,horizon-60, 60,60);                                          // house
-  triangle( 30,horizon-60, 60,horizon-90, 90,horizon-60);
+  fill(245,245,245);
+  rect(40,horizon-50, 15,15);                                          // window
+  rect(65,horizon-50, 15,15);                                          // window
+  fill(255,0,0);
   rect(50,horizon-30, 20,30);                                          // door
-  rect(40,horizon-50, 15,15);
+  triangle( 30,horizon-60, 60,horizon-90, 90,horizon-60);              // roof
+
 
   fill(0);
   text( "My name is Kevin", 10,height-20 );                                          
@@ -49,12 +60,13 @@ void draw() {
 
   /* INSERT YOUR CODE HERE! */
   fill(0,200,200);
-  rect( x,y, 30,50 );        /* REPLACE THIS WITH YOUR OWN CODE! */
-  fill(0,200,200);
+  rect(x-20,y, 70,10);                   // body
+  rect( x,y, 30,50 );        
   ellipse(x+15, y-10, 20, 20);
   fill(255);
-  ellipse(x+10, y-12, 5, 5);
+  ellipse(x+10, y-12, 5, 5);             // eyes
   ellipse(x+20, y-12, 5, 5);
+  arc(x+15,y-7, 10,10, 0, PI, CHORD);    // smile
   text( "Blue", x+3,y+20 );
 
 }
